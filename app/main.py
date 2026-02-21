f4575cf24239b7868f7d1cb64cc80073f1dedb00
Oleksandr Simochko <vhsb721@gmail.com>
2026-02-21 22:00:20 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def copy_file(command: str) -> None:
+    try:
+        cp, file_in, file_out = command.split(" ")
+        if cp != "cp":
+            raise ValueError()
+        if file_in != file_out:
+            with (
+                open(file_in, "r") as file_in,
+                open(file_out, "w") as file_out
+            ):
+                for line in file_in:
+                    file_out.write(line)
+    except ValueError:
+        pass
+    except FileNotFoundError:
+        pass
